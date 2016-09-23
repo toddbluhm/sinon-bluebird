b4b6957a10c0f07241b1fbb461b5c0210ebc18e2
Todd Bluhm <toddbluhm@gmail.com>
2016-09-23 17:28:06 -0400

    Now using JS standard for linting/formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,14 @@
 module.exports = function (grunt) {
-
-  var files = [
-    './*.js*',
-    'test/**/*.js'
-  ];
-
   grunt.initConfig({
-    jshint: {
-      all: files,
+    standard: {
       options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-    jsbeautifier: {
-      modify: {
-        src: files,
-        options: {
-          config: '.jsbeautifyrc'
-        }
+        format: true
+      },
+      app: {
+        src: [
+          '*.js',
+          'test/**/*.js'
+        ]
       }
     },
     mochaTest: {
@@ -31,24 +22,17 @@ module.exports = function (grunt) {
         ]
       }
     }
-  });
+  })
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jsbeautifier');
-  grunt.loadNpmTasks('grunt-mocha-test');
-
-  grunt.registerTask('clean', [
-    'jsbeautifier:modify',
-    'jshint'
-  ]);
+  grunt.loadNpmTasks('grunt-standard')
+  grunt.loadNpmTasks('grunt-mocha-test')
 
   grunt.registerTask('test', [
     'mochaTest'
-  ]);
+  ])
 
   grunt.registerTask('default', [
-    'jshint',
-    'clean',
+    'standard',
     'test'
-  ]);
-};
+  ])
+}
